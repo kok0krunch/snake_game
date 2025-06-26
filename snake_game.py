cd3a9970a048a7f54400f1923b0bf93d12a3480a
kok0krunch <amaliakadoi@gmail.com>
2025-06-27 00:52:06 +0800

    Implement SnakeGame class with initialization of window, canvas, label, score, direction, snake, food, running state, and speed

--- snake_game.py
+++ snake_game.py
@@ -5,3 +5,15 @@ GAME_WIDTH = 700
 GAME_HEIGHT = 700
 SPEED = 100
 SPACE_SIZE = 50
+
+class SnakeGame:
+    def __init__(self, window, canvas, label):
+        self.window = window
+        self.canvas = canvas
+        self.label = label
+        self.score = 0
+        self.direction = 'down'
+        self.snake = Snake(self.canvas)
+        self.food = Food(self.canvas)
+        self.running = True
+        self.speed = SPEED
