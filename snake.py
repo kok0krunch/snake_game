1ec72ba680e42ed11d7421048ceed0f32ef5ea18
kok0krunch <amaliakadoi@gmail.com>
2025-06-27 00:47:18 +0800

    Implement Snake class inheriting from GameObject with initialization of body segments and coordinates

--- snake.py
+++ snake.py
@@ -3,3 +3,13 @@ from game_object import GameObject
 BODY_PARTS = 3
 SPACE_SIZE = 50
 SNAKE_COLOR = "#0FFF07"
+
+class Snake(GameObject):
+    def __init__(self, canvas):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
+        self.canvas = canvas
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append((0, 0))
+            self.squares.append(self.draw_segment(0, 0))
