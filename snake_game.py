b41dd385a1709ae69440493774ed672a9d62f54b
kok0krunch <amaliakadoi@gmail.com>
2025-06-27 00:57:41 +0800

    Add check_collisions method to detect wall and self-collisions for game over logic

--- snake_game.py
+++ snake_game.py
@@ -70,3 +70,17 @@ class SnakeGame:
             self.direction = new_direction
         elif new_direction == 'down' and self.direction != 'up':
             self.direction = new_direction
+    
+    def check_collisions(self):
+        x, y = self.snake.coordinates[0]
+
+        if x < 0 or x >= GAME_WIDTH:
+            return True
+        elif y < 0 or y >= GAME_HEIGHT:
+            return True
+
+        for body_part in self.snake.coordinates[1:]:
+            if x == body_part[0] and y == body_part[1]:
+                return True
+
+        return False
