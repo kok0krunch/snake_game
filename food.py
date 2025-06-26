2165e4fe75f41431243da9f4ab039734747036be
kok0krunch <amaliakadoi@gmail.com>
2025-06-27 00:43:13 +0800

    Add Food class inheriting from GameObject with random placement and drawing initialization

--- food.py
+++ food.py
@@ -5,3 +5,11 @@ GAME_WIDTH = 700
 GAME_HEIGHT = 700
 SPACE_SIZE = 50
 FOOD_COLOR = "#FF0000"
+
+class Food(GameObject):
+    def __init__(self, canvas):
+        x = random.randint(0, (GAME_WIDTH // SPACE_SIZE) - 1) * SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT // SPACE_SIZE) - 1) * SPACE_SIZE
+        super().__init__(canvas, x, y, SPACE_SIZE, FOOD_COLOR)
+        self.coordinates = (x, y)
+        self.oval = self.draw()
